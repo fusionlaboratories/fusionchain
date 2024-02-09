e32e40c25fe77bf495c2ffe0772f4f3b3d5e4a1c
Kris Daniels <kris.daniels@qredo.com>
2024-02-09 17:39:29 +0100

    fix multiuser policy panic

--- blockchain/x/treasury/types/errors.go
+++ blockchain/x/treasury/types/errors.go
@@ -18,5 +18,5 @@ import (
 
 // x/treasury module sentinel errors
 var (
-	ErrSample = sdkerrors.Register(ModuleName, 1100, "sample error")
+	ErrSample = sdkerrors.Register(ModuleName, 1400, "sample error")
 )
